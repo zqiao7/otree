ff0dfcba7bf2c45782c5f3169d33a7d9c4262dc9
Ziqi Qiao <qiao.ziq@gmail.com>
2015-05-27 20:48:48 -0500

    Initiate scs

---
+++ serial_cost_sharing/models.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# <standard imports>
+from __future__ import division
+
+import random
+
+import otree.models
+from otree.db import models
+from otree import widgets
+from otree.common import Currency as c, currency_range, safe_json
+
+# </standard imports>
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+class Constants:
+    name_in_url = 'serial_cost_sharing'
+    players_per_group = None
+    num_rounds = 1
+
+    # define more constants here
+
+
+class Subsession(otree.models.BaseSubsession):
+    pass
+
+
+class Group(otree.models.BaseGroup):
+    # <built-in>
+    subsession = models.ForeignKey(Subsession)
+    # </built-in>
+
+    def set_payoffs(self):
+        for p in self.get_players():
+            p.payoff = 0 # change to whatever the payoff should be
+
+
+class Player(otree.models.BasePlayer):
+    # <built-in>
+    subsession = models.ForeignKey(Subsession)
+    group = models.ForeignKey(Group, null = True)
+    # </built-in>
+
+    def other_player(self):
+        """Returns other player in group. Only valid for 2-player groups."""
+        return self.get_others_in_group()[0]
+
+    # example field
+    my_field = models.CurrencyField(
+        min=c(0),
+        max=c(10),
+        doc="""
+        Description of this field, for documentation
+        """
+    )
+
+
+    def role(self):
+        # you can make this depend of self.id_in_group
+        return ''
---
+++ serial_cost_sharing/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from otree.common import Currency as c, currency_range, safe_json
+
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class MyPage(Page):
+
+    form_model = models.Player
+    form_fields = ['my_field']
+
+    def is_displayed(self):
+        return True
+
+    def vars_for_template(self):
+        return {
+            'my_variable_here': 1,
+        }
+
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        self.group.set_payoffs()
+
+
+class Results(Page):
+
+    pass
+
+
+page_sequence =[
+        MyPage,
+        ResultsWaitPage,
+        Results
+    ]
